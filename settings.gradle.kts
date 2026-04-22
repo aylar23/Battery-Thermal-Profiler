6dcdb43e09bfa7537eff68631507b9863761679f
Aylar <aylarbayram23@belet.me>
2026-04-22 14:28:45 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Battery & Thermal Profiler"
+include(":app")
+include(":core:data")
+include(":core:domain")
+include(":feature:dashboard")
+include(":feature:apps")
+include(":feature:report")
+ 
