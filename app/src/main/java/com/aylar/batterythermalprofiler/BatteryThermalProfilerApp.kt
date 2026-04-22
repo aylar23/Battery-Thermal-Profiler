fb9ac77b6f1dd01eede5edab6ccdb91c17ba036b
Aylar <aylarbayram23@belet.me>
2026-04-22 15:50:14 +0500

    Fix WorkManager Hilt Worker

--- app/src/main/java/com/aylar/batterythermalprofiler/BatteryThermalProfilerApp.kt
+++ app/src/main/java/com/aylar/batterythermalprofiler/BatteryThermalProfilerApp.kt
@@ -1,14 +1,24 @@
 package com.aylar.batterythermalprofiler
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 import com.aylar.batterythermalprofiler.work.WorkScheduler
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BatteryThermalProfilerApp : Application() {
+class BatteryThermalProfilerApp : Application(), Configuration.Provider {
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         WorkScheduler.schedulePeriodicAppPowerCollection(this)
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
 
